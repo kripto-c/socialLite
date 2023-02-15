44cec2d5446280e0066d34e09a504fbf3253d4af
kripto-c <mgutierrezxred@gmail.com>
2023-02-15 02:34:29 -0400

    add shemas.txt

--- sever/dist/config.js
+++ sever/dist/config.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.MONGODB_URI = void 0;
-exports.MONGODB_URI = 'mongodb://127.0.0.1/appGraphqlMern';
+exports.MONGODB_URI = 'mongodb://127.0.0.1/socialLite';
 //# sourceMappingURL=config.js.map
