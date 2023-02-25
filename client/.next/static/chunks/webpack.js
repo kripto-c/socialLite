ea14149d9575f1aed2db6c47056c9bfad9e0f6ac
toledofernando <toledof764@gmail.com>
2023-02-25 01:30:33 -0300

    client scss add

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f83d8007d668a7e1"; }
+/******/ 		__webpack_require__.h = function() { return "c3984cfe3f8eac83"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
